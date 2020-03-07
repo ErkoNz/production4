c6a6860eec414c0ca6d3c6eaa2f3fa461fc53783
ErkoN Z <erik.zabransky@gmail.com>
2020-03-07 23:31:00 +0100

    a

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <div className="App">
         <Nav />
         <Switch>
-          <Route path="/production4/" component={Home} exact />
+          <Route path="/production4" component={Home} exact />
           <Route path="/production4/Currency/CurrencyApp" component={CurrencyApp} />
           <Route path="/TicTacToe/TicTacToe" component={TicTacToe} />
           <Route path="/Todo/TodoApp" component={Todo} />
