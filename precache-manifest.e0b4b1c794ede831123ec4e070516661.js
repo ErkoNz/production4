0e41a80decda131e4404e39f713322d57aeac5bb
ErkoN Z <erik.zabransky@gmail.com>
2020-06-26 23:42:12 +0200

    Updates

---
+++ precache-manifest.e0b4b1c794ede831123ec4e070516661.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39107c45e1f781c1c406618d7328c0c",
+    "url": "/production4/index.html"
+  },
+  {
+    "revision": "67759cdc4eddb5267d67",
+    "url": "/production4/static/css/main.48e812d8.chunk.css"
+  },
+  {
+    "revision": "573927395df229ac1b0b",
+    "url": "/production4/static/js/2.eec374f1.chunk.js"
+  },
+  {
+    "revision": "91209a1236343d0d788eed425951b63b",
+    "url": "/production4/static/js/2.eec374f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67759cdc4eddb5267d67",
+    "url": "/production4/static/js/main.3ca02772.chunk.js"
+  },
+  {
+    "revision": "ac19ee3926b916de4a83",
+    "url": "/production4/static/js/runtime-main.8096dec9.js"
+  },
+  {
+    "revision": "02ed3e1a0edf86764f2afa3faaf816bc",
+    "url": "/production4/static/media/coronaIcon.02ed3e1a.svg"
+  },
+  {
+    "revision": "cdc4522b9dca0855e5f4abb9920d7dc8",
+    "url": "/production4/static/media/earth.cdc4522b.svg"
+  },
+  {
+    "revision": "a29682ddc11eeea095c712775ad8ab65",
+    "url": "/production4/static/media/slovakiaIcon.a29682dd.svg"
+  },
+  {
+    "revision": "d20604a83a6a6e2006af7d3428feb438",
+    "url": "/production4/static/media/wideArrow.d20604a8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/production4/precache-manifest.2d901de6d805a18c9173f1bad51f2edb.js"
+  "/production4/precache-manifest.e0b4b1c794ede831123ec4e070516661.js"
 );
 
 self.addEventListener('message', (event) => {
