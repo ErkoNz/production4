ea99d5c0691dbbecc35f5c7b9c531a22ba4126d4
ErkoNz <60244503+ErkoNz@users.noreply.github.com>
2020-12-19 22:49:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import CovidSK from './Covid/CovidSK';
 import GrafyKrajina from './Covid/GrafyKrajina';
 //asdasd last 22 33
 //master 22 34 add this to new branch
+
+//from github
 function App() {
   return (
     <Router basename="/">
